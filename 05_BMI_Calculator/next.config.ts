7afacb222ed27569e2667d0d0efe5b2e045e043b
krish-vadsak45 <krishvadsak234@gmail.com>
2026-06-12 15:22:40 +0530

    correct config

--- 05_BMI_Calculator/next.config.ts
+++ 05_BMI_Calculator/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_PAGES === "true";
 const repoName = "Koders_Summer_Bootcamp";
-const projectPath = "04_Color_Picker_Tool";
+const projectPath = "05_BMI_Calculator";
 const githubPagesBasePath = `/${repoName}/${projectPath}`;
 
 const nextConfig: NextConfig = {
