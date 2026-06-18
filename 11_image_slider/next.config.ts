19a3f0c60815dddb349d7600f5e07ffd42d12336
krish-vadsak45 <krishvadsak234@gmail.com>
2026-06-18 23:32:20 +0530

    change config

--- 11_image_slider/next.config.ts
+++ 11_image_slider/next.config.ts
@@ -1,18 +1,19 @@
-/** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = "Koders_Summer_Bootcamp";
-const projectPath = "11_Image_Slider";
+import type { NextConfig } from 'next';
+
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'Koders_Summer_Bootcamp';
+const projectPath = '11_Image_Slider';
 const githubPagesBasePath = `/${repoName}/${projectPath}`;
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
   basePath: isGithubPages ? githubPagesBasePath : undefined,
   assetPrefix: isGithubPages ? `${githubPagesBasePath}/` : undefined,
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
