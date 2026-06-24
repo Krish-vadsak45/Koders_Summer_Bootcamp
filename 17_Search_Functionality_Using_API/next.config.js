49bb66f91814f2343b95eb9a1e4f996d7cd1581c
krish-vadsak45 <krishvadsak234@gmail.com>
2026-06-24 23:55:44 +0530

    debugging

--- 17_Search_Functionality_Using_API/next.config.js
+++ 17_Search_Functionality_Using_API/next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === "true";
-const repoName = "Koders_Summer_Bootcamp";
-const projectPath = "17_Search_Functionality_Using_API";
-const githubPagesBasePath = `/${repoName}/${projectPath}`;
 const nextConfig = {
   output: 'export',
+  basePath: '/Koders_Summer_Bootcamp/17_Search_Functionality_Using_API',
+  assetPrefix: '/Koders_Summer_Bootcamp/17_Search_Functionality_Using_API',
   trailingSlash: true,
-  basePath: isGithubPages ? githubPagesBasePath : undefined,
-  assetPrefix: isGithubPages ? `${githubPagesBasePath}/` : undefined,
   images: {
     unoptimized: true,
     remotePatterns: [
